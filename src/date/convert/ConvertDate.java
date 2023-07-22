c6829a8fd2dd57d9d47d7663b3ca8aeb50718e85
zeroh0 <umberto1900@naver.com>
2023-07-22 18:33:56 +0900

    Test: 년월(yyyyMM) 형식으로 입력받았을 때 5개월 전 시작일자와 입력한 년월의 종료일자를 가져온다.

--- src/date/convert/ConvertDate.java
+++ src/date/convert/ConvertDate.java
@@ -27,4 +27,20 @@ public class ConvertDate {
 		assertThat(endDate).isEqualTo(LocalDate.of(2023, 4, 30));
 	}
 
+	@DisplayName("년월(yyyyMM) 형식으로 입력받았을 때 5개월 전 시작일자와 입력한 년월의 종료일자를 가져온다.")
+	@Test
+	void getStartDateAndEndDateFromPreviousFiveMonthToInputMonthWhenInputYearMonth() {
+		// given
+		String input = "202308";
+		YearMonth inputYearMonth = YearMonth.parse(input, DateTimeFormatter.ofPattern("yyyyMM"));
+
+		final int range = 5;
+		LocalDate endDate = inputYearMonth.atEndOfMonth();
+		LocalDate fiveMonthsAgoStartDate = endDate.minusMonths(range - 1).withDayOfMonth(1);
+
+		// when then
+		assertThat(fiveMonthsAgoStartDate).isEqualTo(LocalDate.of(2023, 4, 1));
+		assertThat(endDate).isEqualTo(LocalDate.of(2023, 8, 31));
+	}
+
 }
